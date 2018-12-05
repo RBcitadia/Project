43f77bdc12e59aa7bc54edfe87f166ba8f76d9a2
Remi <rbhagalou@citadia.com>
2018-12-05 14:05:48 +0100

    Création du projet citadia_web

---
+++ django/citadia_web/citadia_web/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def acceuil(request):
+    return HttpResponse("""
+    <h1>BIENVENUE sur CITADIA PLU Collaboratif</h1>
+    <p>Cette plateforme sera le lieu d'échange entre les techniciens des collectivités et les collaborateurs du Groupe Citadia.</p>
+    """)
